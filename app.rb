c4bf5d46ab13dba99b98a5ebcc0e4a5a62edeacd
elioflo <eliofloreschoque2357@gmail.com>
2022-08-09 21:00:09 -0300

    Add next_generation and n_generation methods in GameOfLinfe class

--- app.rb
+++ app.rb
@@ -1,8 +1,5 @@
-class Grid
-    def initialize (column = 0, row = 0)
-        @column = column
-        @row = row
-
+class Board
+    def initialize (row = 0,column = 0)
         @board = []
         row.times do
             @board.push(Array.new(column, '.'))
@@ -10,11 +7,11 @@ class Grid
     end
 
     def column
-        @column
+        @board[1].length
     end
 
     def row
-        @row
+        @board.length
     end
 
     def board
@@ -22,9 +19,20 @@ class Grid
     end
 
     def show_board
+        puts "rows: #{row} columns: #{column}"
         @board.each do |row|
             puts row.join(" ")
         end
+    end
+
+    def is_alive?(position)
+        @board[position.x][position.y] == '*'
+    end
+
+    def is_valid_position?(position)
+        valid_row = position.x >= 0 && position.x <= row-1 
+        valid_column = position.y >= 0 && position.y <= column-1
+        valid_row && valid_column  
     end
 
     def put_alive_cell(rowindex, columnindex)
@@ -43,54 +51,78 @@ class Position
        @y=y
     end
 
-    def X
+    def x
         @x       
-    
     end
+
     def y
         @y             
-    
     end
-
-    
-
 end
 
 
 class GameOfLife
-    def next_generation(board)
-                
+
+    def initialize(board)
+        @board = board
     end
 
-    def alive_neighbours(rowindex,columnindex)
-        alive=0
-        if @board[rowindex][columnindex+1]=="*"{
-            alive+=1
-        }
-        if @board[rowindex][columnindex+1]=="*"{
-            alive+=1
-        }
-        if @board[rowindex][columnindex+1]=="*"{
-            alive+=1
-        }
-        if @board[rowindex][columnindex+1]=="*"{
-            alive+=1
-        }
-        if @board[rowindex][columnindex+1]=="*"{
-            alive+=1
-        }
-        if @board[rowindex][columnindex+1]=="*"{
-            alive+=1
-        }
-        if @board[rowindex][columnindex+1]=="*"{
-            alive+=1
-        }
-        if @board[rowindex][columnindex+1]=="*"{
-            alive+=1
-        }                
+    def setup(cantidad)
     end
 
+    def n_generation(cantidad)
+        cantidad.times do
+            @board.show_board
+            @board = next_generation
+        end
+        @board.show_board
+    end
 
+    def next_generation
+        new_board = Board.new(@board.row,@board.column)
+        @board.board.each_index do |row_index|
+            @board.board[row_index].each_index do |column_index|
+                neighbours = alive_neighbours(row_index,column_index)
+                if @board.is_alive?(Position.new(row_index,column_index))
+                    if neighbours >=2 && neighbours <= 3
+                        new_board.put_alive_cell(row_index,column_index)
+                    else
+                        new_board.put_dead_cell(row_index,column_index)
+                    end
+                else
+                    if neighbours == 3
+                        new_board.put_alive_cell(row_index,column_index)
+                    else
+                        new_board.put_dead_cell(row_index,column_index)
+                    end
+                end
+            end
+        end
+        new_board
+    end
+
+    def alive_neighbours(rowindex,columnindex)                
+        positions = [
+            Position.new(rowindex,columnindex+1),
+            Position.new(rowindex+1,columnindex+1),
+            Position.new(rowindex+1,columnindex),
+            Position.new(rowindex+1,columnindex-1),
+            Position.new(rowindex,columnindex-1),
+            Position.new(rowindex-1,columnindex-1),
+            Position.new(rowindex-1,columnindex),
+            Position.new(rowindex-1,columnindex+1)
+        ]
+
+        neighbours_position = positions.select do |position|
+            @board.is_valid_position?(position)
+        end
+        
+        alive = neighbours_position.select do |position|
+            @board.is_alive?(position)
+        end
+
+        alive.length
+    end
 end
 
 
@@ -100,14 +132,12 @@ end
 
 
 
-instance = Grid.new(8, 4)
-puts instance.column
-puts instance.row
+board_instance = Board.new(4, 8)
+board_instance.put_alive_cell(1,4)
+board_instance.put_alive_cell(2,3)
+board_instance.put_alive_cell(2,4)
+board_instance.put_alive_cell(2,5)
+board_instance.put_alive_cell(3,5)
 
-
-instance.put_alive_cell(1, 4)
-instance.put_alive_cell(2, 3)
-instance.put_alive_cell(2, 4)
-instance.show_board
-
-# comment 
+game_instance = GameOfLife.new(board_instance)
+new_board = game_instance.n_generation(3)
